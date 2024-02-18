f6a6d709c9b2ea8d871822b8b78bff52b94d8069
Villasante Hilares Heli Antonio <hilares33v@gmail.com>
2024-02-18 13:12:42 -0300

    Exception para entidades no encontradas

--- backend/src/Veterinaria.Domain/Repositories/IBasicRepository.cs
+++ backend/src/Veterinaria.Domain/Repositories/IBasicRepository.cs
@@ -1,13 +1,13 @@
 namespace Veterinaria.Domain.Repositories;
 public interface IBasicRepository<TModel, TypeId> where TModel : class
 {
-    Task<List<TModel>> GetAll();
+    Task<List<TModel>> FindAllAsync();
 
-    Task<TModel?> GetById(TypeId id);
+    Task<TModel?> FindByIdAsync(TypeId id);
 
-    Task<TModel> Add(TModel model);
+    Task<TModel> AddAsync(TModel model);
 
-    Task<TModel> Update(TModel model);
+    Task<TModel> UpdateAsync(TModel model);
 
-    Task Delete(TModel model);
+    Task DeleteAsync(TModel model);
 }
