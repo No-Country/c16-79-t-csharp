55912c845262d61df905912307fcd4ba6ae8bc65
Villasante Hilares Heli Antonio <hilares33v@gmail.com>
2024-02-18 13:27:13 -0300

    correccion de ResourceNotFoundException

--- backend/src/Veterinaria.Application/CustomeExceptions/ResourceNotFoundException.cs
+++ backend/src/Veterinaria.Application/CustomeExceptions/ResourceNotFoundException.cs
@@ -7,7 +7,7 @@ public class ResourceNotFoundException : Exception
     public ResourceNotFoundException() { }
     public ResourceNotFoundException(string message) : base(message) { }
     public ResourceNotFoundException(string message, Exception inner) : base(message, inner) { }
-    public static ResourceNotFoundException NotFoundById<TModel, TypeId>(TModel model, TypeId id) where TModel : class
+    public static ResourceNotFoundException NotFoundById<TModel, TypeId>(TypeId id) where TModel : class
     {
         return new ResourceNotFoundException($"The {nameof(TModel)} with ID {id} was not found.");
     }
