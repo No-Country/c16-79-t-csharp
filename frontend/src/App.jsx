8f79fe632abd6fb91df6ee42e52e07589022ebac
Cari Costa <cari.costa90@gmail.com>
2024-02-20 16:59:45 -0300

    Se agrega banner de ofertas homePage - correcciones de estilo en footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,15 @@
-
-import './App.css'
-import NavBar from './Common/NavBar'
-import {FooterComponent}   from './Common/FooterComponent' 
-
+import "./App.css";
+import NavBar from "./Common/NavBar";
+import { FooterComponent } from "./Common/FooterComponent";
+import { CarouselBanner } from "./Components/Carousel/CarouselBanner";
 function App() {
-
-
   return (
     <>
       <NavBar />
+      <CarouselBanner></CarouselBanner>
       <FooterComponent></FooterComponent>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
