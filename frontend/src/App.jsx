e4f87c0bbf4f6f0f4107d9b4adc7fe9265eaab0d
Ale Fernandez <alejandro.d.fernandez.88@gmail.com>
2024-02-20 17:20:23 -0300

    50% carusel marcas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@
 import './App.css'
 import NavBar from './Common/NavBar'
 import {FooterComponent}   from './Common/FooterComponent' 
+import CarrouselMarcas from './Components/CarrouselMarcas/CarrouselMarcas'
 
 function App() {
-
 
   return (
     <>
       <NavBar />
+      <CarrouselMarcas/>
       <FooterComponent></FooterComponent>
     </>
   )
---
+++ frontend/src/assets/marcas.js
@@ -0,0 +1,36 @@
+const marcas = [
+  {
+    title: "Royal Canin",
+    img: "https://puppis.vteximg.com.br/arquivos/ids/163028/Royal%20Canin%20150x150.png?v=636613090416470000",
+  },
+  {
+    title: "Vital can",
+    img: "https://puppis.vteximg.com.br/arquivos/ids/163030/VitalCan%20150x150.png?v=636613090441530000",
+  },
+  {
+    title: "Eukanuba",
+    img: "https://puppis.vteximg.com.br/arquivos/ids/163020/Eukanuba%20150x150.png?v=636764384009700000",
+  },
+  {
+    title: "Royal Canin",
+    img: "https://puppis.vteximg.com.br/arquivos/ids/163021/Excellent%20150x150.png?v=636613090346730000",
+  },
+  {
+    title: "Excellent",
+    img: "https://puppis.vteximg.com.br/arquivos/ids/163028/Royal%20Canin%20150x150.png?v=636613090416470000",
+  },
+  {
+    title: "Old price",
+    img: "https://puppis.vteximg.com.br/arquivos/ids/176336/Old%20Prince%20150x150_1.png?v=637535916472600000",
+  },
+  {
+    title: "Proplan",
+    img: "https://puppis.vteximg.com.br/arquivos/ids/191410/Web_LogoPurinaPloplan_OUT-1.png?v=638107735093430000",
+  },
+  {
+    title: "Pawise",
+    img: "https://puppis.vteximg.com.br/arquivos/ids/185679/pawise-logo-marcas.png?v=637830354377100000",
+  },
+];
+
+export default marcas
