410abbabab29ab82993f8aa99f10789cf3186cee
Teche95 <silencertec@gmail.com>
2024-02-20 17:59:57 -0300

    agregados banner y productos destacados

--- frontend/src/Components/Ofertas/Ofertas.jsx
+++ frontend/src/Components/Ofertas/Ofertas.jsx
@@ -7,14 +7,14 @@ const Ofertas = () => {
 
   return (
     <Banner>
-      <div className="flex w-full justify-between border-t border-gray-200 bg-gray-100 p-4 dark:border-gray-600 dark:bg-gray-700">
+      <div className="flex w-full justify-between border-t border-gray-200 bg-purple-400 p-4 dark:border-gray-600 dark:bg-gray-700">
         <div className="mx-auto flex items-center">
-          <p className="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
-            <span className="mr-3 inline-flex h-6 w-6 items-center justify-center rounded-full bg-gray-200 p-1 dark:bg-gray-600">
+          <p className="flex items-center text-sm font-normal text-white">
+            <span className="mr-3 inline-flex h-6 w-6 items-center justify-center rounded-full bg-black p-1 dark:bg-gray-600">
               <MdPercent className="h-4 w-4" />
             </span>
             <span className="[&_p]:inline">
-              OBETENGA UN 25% DE DESCUENTO EN NUESTRA&nbsp;
+              OBETENGA UN <strong>25%</strong> DE DESCUENTO EN NUESTRA&nbsp;
               <a
                 href="https://flowbite.com"
                 className="ml-0 flex items-center text-sm font-medium text-cyan-600 hover:underline dark:text-cyan-500 md:ml-1 md:inline-flex"
@@ -25,9 +25,9 @@ const Ofertas = () => {
             </span>
           </p>
         </div>
-        <Banner.CollapseButton color="gray" className="border-0 bg-transparent text-gray-500 dark:text-gray-400">
+        {/* <Banner.CollapseButton color="gray" className="border-0 bg-transparent text-gray-500 dark:text-gray-400">
           <HiX className="h-4 w-4" />
-        </Banner.CollapseButton>
+        </Banner.CollapseButton> */}
       </div>
     </Banner>
   )
--- frontend/src/Components/ProductosDestacados/ProductoDestacados.jsx
+++ frontend/src/Components/ProductosDestacados/ProductoDestacados.jsx
@@ -9,19 +9,14 @@ const ProductoDestacados = () => {
   return (
     <>
       <h1 className='text-center text-4xl'>Destacados</h1>
-      <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center ">
-        {/* {
-          data.map(il => (
-            <h1>{il.nombre}</h1>
-          ))
-        } */}
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center w-4/4  gap-200-px">
+
         {data.map((producto, index) => (
           <Card
-            className="mb-5 w-72"
+            className="mb-5 "
             key={index}
             imgAlt={producto.nombre}
             imgSrc={producto.imagen}
-
           >
             <a href="#">
               <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
@@ -31,11 +26,11 @@ const ProductoDestacados = () => {
             <p className="text-gray-700 dark:text-gray-300">{producto.descripcion}</p>
             {/* <p className="text-gray-700 dark:text-gray-300">Categoría: {producto.categoria}</p> */}
             {/* <p className="text-gray-700 dark:text-gray-300">Stock: {producto.stock}</p> */}
-            <div className="flex items-center ">
+            <div className="flex flex-col items-center ">
               <span className="text-3xl font-bold text-gray-900 dark:text-white">${producto.precio}</span>
               <a
                 href="#"
-                className="ml-auto rounded-lg bg-cyan-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
+                className="rounded-lg bg-cyan-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
               >
                 Add to cart
               </a>
