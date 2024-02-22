cd915717784265a3076e988950789977e4cd3126
Teche95 <silencertec@gmail.com>
2024-02-22 18:42:34 -0300

    solucionando problemas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,14 @@
 import './App.css'
 import NavBar from './Common/NavBar'
 import { FooterComponent } from './Common/FooterComponent'
-import ProductoDestacados from './Components/ProductosDestacados/ProductoDestacados'
-import Ofertas from './Components/Ofertas/Ofertas'
-import { CarouselBanner } from "./Components/Carousel/CarouselBanner";
-import CarrouselMarcas from './Components/CarrouselMarcas/CarrouselMarcas'
-import Testimonios from "./Components/Testimonios/Testimonios";
-
+import { Router } from './Router';
 
 
 function App() {
   return (
     <>
       <NavBar />
-      <Router/> 
+      <Router />
       <FooterComponent />
     </>
   );
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,16 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-// import Perfil from './Pages/Perfil/Perfil.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
-    <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </BrowserRouter>
-  </>
-
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 )
