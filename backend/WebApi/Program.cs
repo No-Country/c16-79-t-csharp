b1eb683a68a462f0f9b167213360a1355450f9f8
Villasante Hilares Heli Antonio <hilares33v@gmail.com>
2024-02-22 18:50:21 -0300

    controllers agregado a al builder

--- backend/WebApi/Program.cs
+++ backend/WebApi/Program.cs
@@ -14,6 +14,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
+builder.Services.AddControllers();
+
 builder.Services.AddDependencyInfrastructure(builder.Configuration);
 
 builder.Services.AddAuthorization();
@@ -103,6 +105,8 @@ app.MapHealthChecks("/hc", new HealthCheckOptions()
     Predicate = _ => true,
     ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
 });
+
+app.MapControllers();
 
 app.UseAuthentication();
 app.UseAuthorization();
