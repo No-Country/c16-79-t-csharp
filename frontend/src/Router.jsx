02bf0dcd77cd0b4dda3a23de7bce46d4043e0fca
Ale Fernandez <alejandro.d.fernandez.88@gmail.com>
2024-02-22 16:59:52 -0300

    Solucion merge rutas

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -4,13 +4,15 @@ import { Tienda } from "./Pages/Tienda/Tienda";
 import { Adoptar } from "./Pages/Adoptar/Adoptar";
 import { Agenda } from "./Pages/Agenda/Agenda";
 import { Servicios } from "./Pages/Servicios/Servicios";
-import { PerdidosEncontrados } from "./Pages/Perdidos-Econtrados/PerdidosEncontrados";
+import { PerdidosEncontrados } from "./Pages/Perdidos-Encontrados/PerdidosEncontrados";
 import { QuieneSomos } from "./Pages/QuienesSomos/QuieneSomos";
+import { Perfil } from './Pages/Perfil/Perfil'
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/perfil" element={<Perfil/>} />
       <Route path="/tienda" element={<Tienda />} />
       <Route path="/adoptar" element={<Adoptar />} />
       <Route path="/agenda" element={<Agenda />} />
