f8cc620ea569ac14c91c77f00ad7b78f63b9d768
Cari Costa <cari.costa90@gmail.com>
2024-02-27 16:42:20 -0300

    Se agrega toast a agenda y se caomodan estilos

--- frontend/src/Pages/Agenda/Agenda.jsx
+++ frontend/src/Pages/Agenda/Agenda.jsx
@@ -1,13 +1,18 @@
 
 import { SeleccioneSuMascota } from "../../Components/SeleccioneSuMacosta/SeleccioneSuMascota"
 import SeleccionEstudio from '../../Components/SeleccionEstudio/SeleccionEstudio'
+import { ToastAgenda } from "../../Components/ToastAgenda/ToastAgenda"
 
 
 export const Agenda = () => {
   return (
     <div>
+      <h1 className="py-5 mb-4 text-2xl font-extrabold leading-none tracking-tight text-center text-gray-500 md:text-5xl lg:text-4xl dark:text-white">Agenda</h1>
+      <div className="flex place-content-around mb-40 mt-16">
       <SeleccioneSuMascota />
       <SeleccionEstudio/>
+      </div>
+      <ToastAgenda />
     </div>
   )
 }
