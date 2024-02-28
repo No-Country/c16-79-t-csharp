0f7b935c5b339afd05df8320136d3392442fad42
Ale Fernandez <alejandro.d.fernandez.88@gmail.com>
2024-02-27 22:47:57 -0300

    Corrigiendo despues del super merge

--- frontend/src/Pages/Agenda/Agenda.jsx
+++ frontend/src/Pages/Agenda/Agenda.jsx
@@ -6,13 +6,13 @@ import { SelecFechaHoraTurno } from "../../Components/SelecFechaHoraTurno/SelecF
 
 export const Agenda = () => {
   return (
-    <div>
+    <div className="container w-4/5 mx-auto">
       <h1 className="py-5 mb-4 text-2xl font-extrabold leading-none tracking-tight text-center text-gray-500 md:text-5xl lg:text-4xl dark:text-white">
         Agenda
       </h1>
-      <div className="flex place-content-around mb-40 mt-16">
-        <SeleccioneSuMascota />
-        <SeleccionEstudio />
+      <div className="flex place-content-around mb-10 mt-16 flex-wrap gap-2">
+        <SeleccioneSuMascota className=" w-2/5"/>
+        <SeleccionEstudio className=" w-2/5 "/>
         <SelecFechaHoraTurno />
       </div>
       <ToastAgenda />
