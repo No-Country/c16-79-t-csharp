6c4377d4d2408c21ae2f9fac0567ae111ea6d970
kamilo042 <kamilo042.cp@gmail.com>
2024-02-29 20:14:11 -0500

    datos usuario

--- frontend/src/Components/DatosUsuario/DatosUsuario.jsx
+++ frontend/src/Components/DatosUsuario/DatosUsuario.jsx
@@ -26,11 +26,14 @@ const DatosUsuario = () => {
     <div className="overflow-x-auto container w-4/5 mx-auto mt-10 mb-10">
       <Table>
         <Table.Body className="divide-y">
+
+
+
           <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
             <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
               Nombre
             </Table.Cell>
-            <Table.Cell>{first?.data.name}</Table.Cell>
+            <Table.Cell>{first?.data.lastName}</Table.Cell>
             <Table.Cell>
               <a
                 href="#"
@@ -40,26 +43,11 @@ const DatosUsuario = () => {
               </a>
             </Table.Cell>
           </Table.Row>
-          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              Nombre Usuario
-            </Table.Cell>
-            <Table.Cell>{first?.data.userName}</Table.Cell>
-            <Table.Cell>
-              <a
-                href="#"
-                className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
-              >
-                Edit
-              </a>
-            </Table.Cell>
-          </Table.Row>
-
           <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
             <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
               Apellido
             </Table.Cell>
-            <Table.Cell>{first?.data.lastName}</Table.Cell>
+            <Table.Cell>{first?.data.phoneNumber}</Table.Cell>
             <Table.Cell>
               <a
                 href="#"
