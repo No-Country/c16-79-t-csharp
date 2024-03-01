104071044da70fb268becc064d46f4165d1e8c72
Cari Costa <cari.costa90@gmail.com>
2024-03-01 15:41:01 -0300

    merge de ramas

--- frontend/src/Helpers/useFetch.js
+++ frontend/src/Helpers/useFetch.js
@@ -1,73 +1,59 @@
 import { useEffect, useState } from "react";
 
 export const useFetchGet = async (endPoint) => {
-
-  // console.log("endpoint", endPoint)
   try {
-    const response = await fetch(`http://localhost:4600/${endPoint}`, {
-      // method: method ? method : "GET",
-      headers: {
-        "Authorization": localStorage.getItem("token") ? `Bearer ${localStorage.getItem("token")}` : ""
-      }
-    });
+    const response = await fetch(`http://localhost:4600/"${endPoint}`, {
+      headers: { 
+      "Authentication" : localStorage.getItem("token") ? `Bearer ${localStorage.getItem("token")}` : ""
+    }});
     const data = await response.json();
-    // console.log("data: ", data);
-    return data;
-
+    console.log("data: ", data);
   } catch (error) {
     console.log("Error: " + error);
   }
 };
 
-
 export const useFetchPost = (endPoint, input) => {
 
-  // console.log(method)
+
   const [state, setState] = useState({
     data: "",
   });
 
+ 
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://localhost:4600/${endPoint}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",            
+            "Authentication" : localStorage.getItem("token") ? `Bearer ${localStorage.getItem("token")}` : ""
+          },
+          body: JSON.stringify(input),
+        });
+        const data = await response.json();
+        localStorage.setItem("token", data.token);
+        setState({
+          data,
+        });
 
-  const fetchData = async () => {
-    console.log("token1", localStorage.getItem("token"))
-    try {
-
-      const response = await fetch(`http://localhost:4600/${endPoint}`, {
-        // method: method ? method : "GET",
-        // method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": localStorage.getItem("token") ? `Bearer ${localStorage.getItem("token")}` : ""
-        },
-        body: JSON.stringify(input),
-      });
-      const data = await response.json();
-      localStorage.setItem("token", data.token);
-      setState({
-        data,
-      });
-
-    } catch (error) {
-      setState({
-        data: "",
-      });
-    }
-  };
-
-  useEffect(() => {
-    if (!endPoint) {
-      return
-    }
-    fetchData();
-  }, [])
-
-  console.log("token2", localStorage.getItem("token"))
+      } catch (error) {
+        setState({
+          data: "",
+        });
+      }
+    };
+    
+    useEffect(() => {
+      if(!endPoint){
+        return
+      }
+      fetchData();
+      //eslint-disable-next-line
+    }, [])
+    
   return {
-    state,
+   state,
     fetchData
   };
-
-};
-
-
-//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJiM2RlMTY5ZC1jZjYxLTQ0M2MtYTNjYS1iYWE2MzBkZTljYzkiLCJlbWFpbCI6InBydWViYTEzQHBydWViYTIuY29tIiwicm9sZSI6IkNsaWVudGUiLCJuYmYiOjE3MDkyMzEyNjEsImV4cCI6MTcwOTMxNzY2MSwiaWF0IjoxNzA5MjMxMjYxfQ.cd7ZXcmVxFv4DviGl-McOZLAnvIsHXGIUf2YuioS7yw
+};    
