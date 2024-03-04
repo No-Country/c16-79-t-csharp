bff90960ba2238a659562bf04380601d08934486
Cari Costa <cari.costa90@gmail.com>
2024-03-04 14:40:53 -0300

    se traen mascotas a componente agenda desde base de datos

--- frontend/src/Components/SeleccioneSuMacosta/SeleccioneSuMascota.jsx
+++ frontend/src/Components/SeleccioneSuMacosta/SeleccioneSuMascota.jsx
@@ -1,14 +1,43 @@
-'use client';
+/* eslint-disable react-hooks/exhaustive-deps */
+"use client";
 
-import { Dropdown } from 'flowbite-react';
+import { Dropdown } from "flowbite-react";
+import { useFetchGet } from "../../Helpers/useFetch";
+import { useEffect, useState } from "react";
 
 export const SeleccioneSuMascota = () => {
-    return (
-        <div>
-            <Dropdown label="Seleccione su mascota" dismissOnClick={false}>
-                <Dropdown.Item>Mascota 1</Dropdown.Item>
-                <Dropdown.Item>Mascota 2</Dropdown.Item>
-            </Dropdown>
-        </div>
-    )
-}
+  const [info, setInfo] = useState([]);
+
+  console.log("info", info);
+
+  const { fetchData } = useFetchGet("api/ClientUsers/me/pets");
+
+  useEffect(() => {
+    const handleDatos = async () => {
+      if (localStorage.getItem("token")) {
+        try {
+          const respuesta = await fetchData();
+          const pruebaRes = respuesta.data;
+          setInfo(pruebaRes);
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      }
+    };
+    handleDatos();
+  }, []);
+
+  return (
+    <div>
+      <Dropdown label="Seleccione su mascota" dismissOnClick={false}>
+        {info.length > 0 ? (
+          info?.map((d) => {
+            return <Dropdown.Item key={d.id}>{d.name}</Dropdown.Item>;
+          })
+        ) : (
+            <Dropdown.Item >No tiene mascotas cargadas </Dropdown.Item>
+        )}
+      </Dropdown>
+    </div>
+  );
+};
