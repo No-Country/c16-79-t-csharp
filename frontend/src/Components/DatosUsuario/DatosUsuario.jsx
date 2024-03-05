08ba5d08292e05d721a2f2bf579916222ad87f55
Ale Fernandez <alejandro.d.fernandez.88@gmail.com>
2024-03-05 02:09:13 -0300

    Editar datos de usuario y perfil completados

--- frontend/src/Components/DatosUsuario/DatosUsuario.jsx
+++ frontend/src/Components/DatosUsuario/DatosUsuario.jsx
@@ -4,25 +4,23 @@ import { useEffect, useState } from "react";
 import { EdicionDatosUsuario } from "./EdicionDatosUsuario";
 
 const DatosUsuario = () => {
+  const [first, setfirst] = useState();
 
-  const [first, setfirst] = useState()
+  console.log("first", first);
 
-  console.log("first", first)
-
-  const { fetchData } = useFetchGet("api/ClientUsers/me")
+  const { fetchData } = useFetchGet("api/ClientUsers/me");
 
   useEffect(() => {
     const handleDatos = async () => {
       if (localStorage.getItem("token")) {
         try {
-          const data = await fetchData()
+          const data = await fetchData();
           console.log("Data received:", data);
-          setfirst(data)
+          setfirst(data);
         } catch (error) {
           console.error("Error fetching data:", error);
         }
       }
-
     };
     handleDatos();
     //eslint-disable-next-line
@@ -32,69 +30,38 @@ const DatosUsuario = () => {
     <div className="overflow-x-auto container w-4/5 mx-auto mt-10 mb-10">
       <Table>
         <Table.Body className="divide-y">
-
-
-
           <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
             <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
               Nombre
             </Table.Cell>
             <Table.Cell>{first?.data?.name}</Table.Cell>
-            <Table.Cell>
-              <a
-                href="#"
-                className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
-              >
-                Edit
-              </a>
-            </Table.Cell>
           </Table.Row>
           <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
             <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
               Nombre Usuario
             </Table.Cell>
             <Table.Cell>{first?.data?.userName}</Table.Cell>
-            <Table.Cell>
-              <a
-                href="#"
-                className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
-              >
-                Edit
-              </a>
-            </Table.Cell>
           </Table.Row>
-
           <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
             <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
               Apellido
             </Table.Cell>
             <Table.Cell>{first?.data?.lastName}</Table.Cell>
-            <Table.Cell>
-              <a
-                href="#"
-                className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
-              >
-                Edit
-              </a>
-            </Table.Cell>
           </Table.Row>
           <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
             <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              Telefono
+              Teléfono
             </Table.Cell>
             <Table.Cell>{first?.data?.phoneNumber}</Table.Cell>
-            <Table.Cell>
-              <a
-                href="#"
-                className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
-              >
-                Edit
-              </a>
-            </Table.Cell>
           </Table.Row>
         </Table.Body>
       </Table>
-      <EdicionDatosUsuario nombre2={first?.data?.name} apellido={first?.data?.lastName} nombre={first?.data?.userName} telefono={first?.data?.phoneNumber} />
+      <EdicionDatosUsuario
+        nombre2={first?.data?.name}
+        apellido={first?.data?.lastName}
+        nombre={first?.data?.userName}
+        telefono={first?.data?.phoneNumber}
+      />
     </div>
   );
 };
--- frontend/src/Components/DatosUsuario/EdicionDatosUsuario.jsx
+++ frontend/src/Components/DatosUsuario/EdicionDatosUsuario.jsx
@@ -2,15 +2,14 @@
 /* eslint-disable react/prop-types */
 "use client";
 
-import { Button, Checkbox, Label, Modal, TextInput } from "flowbite-react";
+import { Button, Label, Modal, TextInput } from "flowbite-react";
 import { useState, useRef, useEffect } from "react";
 import { useFetchPut } from "../../Helpers/useFetch";
-
 
 export const EdicionDatosUsuario = ({
   nombre,
   telefono,
-  nombre2,
+  Usuario,
   apellido,
 }) => {
   // usamos useState para controlar el estado del input del formulario.
@@ -22,7 +21,9 @@ export const EdicionDatosUsuario = ({
   });
 
   //usamos useref para referenciar el campo que vamos a cambiar(hablando de los inputs del modal)
-  const inputRefname = useRef(null);
+  const inputRefName = useRef(null);
+  const inputRefApellido = useRef(null);
+  const inputRefUserName = useRef(null);
   const inputReftel = useRef(null);
 
   const [openModal, setOpenModal] = useState(false);
@@ -32,12 +33,12 @@ export const EdicionDatosUsuario = ({
 
   //funcion dentro del  onClick para guardar los datos editados
   const editarDatos = async () => {
-    console.log("lognuevo", usuarioValue, telefonoValue, nombre2, apellido);
+    console.log("lognuevo", usuarioValue, telefonoValue, nombreValue, apellidoValue);
     //pasa los campos editables al body del custom hook
     setInput((prevInput) => ({
       ...prevInput,
-      name: nombre2,
-      lastName: apellido,
+      name: nombreValue,
+      lastName: apellidoValue,
       userName: usuarioValue,
       phoneNumber: telefonoValue,
     }));
@@ -56,21 +57,25 @@ export const EdicionDatosUsuario = ({
     }
   }, [input]);
 
-
   //declaro variables para los campos editables
-  let telefonoValue;
+  let nombreValue;
+  let apellidoValue;
   let usuarioValue;
+  let telefonoValue;
+
   const actualizarDatos = () => {
     //este el onchange que lee el valor de los inputs despues de haberlos cambiado y los guarda en las variables que declare arriba
+    nombreValue = inputRefName.current.value;
+    apellidoValue = inputRefApellido.current.value;
+    usuarioValue = inputRefUserName.current.value;
     telefonoValue = inputReftel.current.value;
-    usuarioValue = inputRefname.current.value;
   };
 
   function onCloseModal() {
     setOpenModal(false);
   }
   return (
-    <div>
+    <div className=" mt-5">
       <Button onClick={() => setOpenModal(true)}>Editar datos</Button>
       <Modal show={openModal} size="md" onClose={onCloseModal} popup>
         <Modal.Header />
@@ -79,28 +84,57 @@ export const EdicionDatosUsuario = ({
             <h3 className="text-xl font-medium text-gray-900 dark:text-white">
               Edición de datos
             </h3>
-
             <div>
               <div className="mb-2 block">
-                <Label htmlFor="email" value="Nombre Usuario" />
+                <Label htmlFor="nombre" value="Nombre" />
               </div>
               <TextInput
-                id="email"
-                placeholder="name@company.com"
+                id="nombre"
+                placeholder="Nombre"
+                type="text"
+                required
                 defaultValue={nombre}
+                name="nombre"
+                onChange={actualizarDatos}
+                ref={inputRefName}
+              />
+            </div>
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="text" value="Apellido" />
+              </div>
+              <TextInput
+                id="apellido"
+                placeholder="Apellido"
+                type="text"
+                required
+                defaultValue={apellido}
+                name="apellido"
+                onChange={actualizarDatos}
+                ref={inputRefApellido}
+              />
+            </div>
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="text" value="Nombre Usuario" />
+              </div>
+              <TextInput
+                id="nombreusuario"
+                placeholder="Nombre de usuario"
+                defaultValue={Usuario}
                 onChange={actualizarDatos}
                 required
                 name="nombreusuario"
-                ref={inputRefname}
+                ref={inputRefUserName}
               />
             </div>
-
             <div>
               <div className="mb-2 block">
-                <Label htmlFor="password" value="Teléfono" />
+                <Label htmlFor="text" value="Teléfono" />
               </div>
               <TextInput
-                id="password"
+                id="telefono"
+                placeholder="Telefono"
                 type="text"
                 required
                 defaultValue={telefono}
@@ -109,8 +143,7 @@ export const EdicionDatosUsuario = ({
                 ref={inputReftel}
               />
             </div>
-
-            <div className="flex justify-between">
+            {/* <div className="flex justify-between">
               <div className="flex items-center gap-2">
                 <Checkbox id="remember" />
                 <Label htmlFor="remember">Remember me</Label>
@@ -121,13 +154,11 @@ export const EdicionDatosUsuario = ({
               >
                 Lost Password?
               </a>
-            </div>
-
+            </div> */}
             <div className="w-full">
               <Button onClick={editarDatos}>Guardar cambios</Button>
             </div>
-
-            <div className="flex justify-between text-sm font-medium text-gray-500 dark:text-gray-300">
+            {/* <div className="flex justify-between text-sm font-medium text-gray-500 dark:text-gray-300">
               Not registered?&nbsp;
               <a
                 href="#"
@@ -135,7 +166,7 @@ export const EdicionDatosUsuario = ({
               >
                 Create account
               </a>
-            </div>
+            </div> */}
           </div>
         </Modal.Body>
       </Modal>
