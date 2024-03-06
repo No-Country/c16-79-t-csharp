b1da10536a44b04dde10dc10899a78a1abf6fa1f
kamilo042 <kamilo042.cp@gmail.com>
2024-03-06 18:54:59 -0500

    historial medico terminado

--- frontend/src/Components/HistorialMedico/HistorialMedico.jsx
+++ frontend/src/Components/HistorialMedico/HistorialMedico.jsx
@@ -1,11 +1,11 @@
-import { Accordion } from 'flowbite-react';
+import {  Card } from 'flowbite-react';
 import { useEffect, useState } from "react";
 import { useFetchGet } from '../../Helpers/useFetch';
 
 export const HistorialMedico = () => {
   const [historialMedico, setHistorialMedico] = useState([]);
 
-  const { fetchData } = useFetchGet("api/MedicalHistories");
+  const { fetchData } = useFetchGet("api/ClientUsers/me/pets/medicalhistories");
 
   useEffect(() => {
     const handleDatos = async () => {
@@ -24,31 +24,34 @@ export const HistorialMedico = () => {
   }, []);
 
   return (
-    <>
-      <Accordion className='container w-4/5 mx-auto mt-10 mb-10'>
-        {historialMedico.slice(0,1).map((item, index) => (
-          <Accordion.Panel key={index}>
-            <Accordion.Title>Diagnóstico</Accordion.Title>
-            <Accordion.Content>
-              <p className="mb-2 text-gray-500 dark:text-gray-400">
-                {item.diagnostic}
-              </p>
-            </Accordion.Content>
-            <Accordion.Title>Médico Veterinario</Accordion.Title>
-            <Accordion.Content>
-              <p className="mb-2 text-gray-500 dark:text-gray-400">
-                {item.medic}
-              </p>
-            </Accordion.Content>
-            <Accordion.Title>Fecha</Accordion.Title>
-            <Accordion.Content>
-              <p className="mb-2 text-gray-500 dark:text-gray-400">
-                {item.time}
-              </p>
-            </Accordion.Content>
-          </Accordion.Panel>
-        ))}
-      </Accordion>
-    </>
+  
+    <div className='container flex gap-10 mx-auto flex-wrap justify-center'>
+    {historialMedico.map((item, index) => (
+      <Card key={index}  className="max-w-sm bg-[radial-gradient(ellipse_80%_80%at_50%-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+                
+        <p className="font-bold text-xl text-gray-700 dark:text-gray-400 text-center">
+          {item.petName}
+        </p> 
+        <h5 className="text-base  tracking-tight text-gray-600 dark:text-white">
+          Diagnóstico
+        </h5>
+        <p className="font-normal text-lg text-gray-800 dark:text-gray-800">
+          {item.diagnostic}
+        </p>
+        <h5 className="text-base  tracking-tight text-gray-600 dark:text-white">
+          Médico Veterinario
+        </h5>
+        <p className="font-normal text-lg text-gray-700 dark:text-gray-800">
+          {item.medic}
+        </p>
+        <h5 className="text-base tracking-tight text-gray-600 dark:text-white">
+          Fecha
+        </h5>
+        <p className="font-normal text-lg text-gray-700 dark:text-gray-800">
+          {(new Date(item.time)).toLocaleDateString()}
+        </p>        
+      </Card>
+    ))}
+    </div>
   );
 };
