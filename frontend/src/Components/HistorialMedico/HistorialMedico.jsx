30e29067fc8aa5cbdddfaa8e5399462df529e980
kamilo042 <kamilo042.cp@gmail.com>
2024-03-06 16:07:35 -0500

    historial medico render datos de db

--- frontend/src/Components/HistorialMedico/HistorialMedico.jsx
+++ frontend/src/Components/HistorialMedico/HistorialMedico.jsx
@@ -1,61 +1,54 @@
 import { Accordion } from 'flowbite-react';
+import { useEffect, useState } from "react";
+import { useFetchGet } from '../../Helpers/useFetch';
 
 export const HistorialMedico = () => {
+  const [historialMedico, setHistorialMedico] = useState([]);
+
+  const { fetchData } = useFetchGet("api/MedicalHistories");
+
+  useEffect(() => {
+    const handleDatos = async () => {
+      if (localStorage.getItem("token")) {
+        try {
+          const data = await fetchData();
+          setHistorialMedico(data.data); // Assuming 'data' is an array
+          console.log(data.data[0].diagnostic, "historial medico");
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      }
+    };
+    handleDatos();
+    //eslint-disable-next-line
+  }, []);
+
   return (
     <>
-    <Accordion className='container w-4/5 mx-auto mt-10 mb-10'>
-      <Accordion.Panel>
-        <Accordion.Title>Vacunas</Accordion.Title>
-        <Accordion.Content>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">
-          Tipo de vacuna, fecha de administración, próxima dosis
-          </p>
-
-        </Accordion.Content>
-      </Accordion.Panel>
-      <Accordion.Panel>
-        <Accordion.Title>Medicamentos</Accordion.Title>
-        <Accordion.Content>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">
-          Nombre del medicamento, dosis, frecuencia, motivo de prescripción
-          </p>
-        </Accordion.Content>
-      </Accordion.Panel>
-      <Accordion.Panel>
-        <Accordion.Title>Cirugías o procedimientos realizados</Accordion.Title>
-        <Accordion.Content>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">
-          Descripción, fecha
-          </p>
-        </Accordion.Content>
-      </Accordion.Panel>
-      <Accordion.Panel>
-        <Accordion.Title>Enfermedades previas</Accordion.Title>
-        <Accordion.Content>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">
-          Descripción, tratamiento realizado
-          </p>
-
-        </Accordion.Content>
-      </Accordion.Panel>
-      <Accordion.Panel>
-        <Accordion.Title>Tratamientos antiparasitarios</Accordion.Title>
-        <Accordion.Content>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">
-          Tipo de tratamiento, fecha de administración, próxima dosis
-          </p>
-        </Accordion.Content>
-      </Accordion.Panel>
-      <Accordion.Panel>
-        <Accordion.Title>Notas Adicionales</Accordion.Title>
-        <Accordion.Content>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">
-          Notas adicionales o comentarios
-          </p>
-
-        </Accordion.Content>
-      </Accordion.Panel>
-    </Accordion>
+      <Accordion className='container w-4/5 mx-auto mt-10 mb-10'>
+        {historialMedico.slice(0,1).map((item, index) => (
+          <Accordion.Panel key={index}>
+            <Accordion.Title>Diagnóstico</Accordion.Title>
+            <Accordion.Content>
+              <p className="mb-2 text-gray-500 dark:text-gray-400">
+                {item.diagnostic}
+              </p>
+            </Accordion.Content>
+            <Accordion.Title>Médico Veterinario</Accordion.Title>
+            <Accordion.Content>
+              <p className="mb-2 text-gray-500 dark:text-gray-400">
+                {item.medic}
+              </p>
+            </Accordion.Content>
+            <Accordion.Title>Fecha</Accordion.Title>
+            <Accordion.Content>
+              <p className="mb-2 text-gray-500 dark:text-gray-400">
+                {item.time}
+              </p>
+            </Accordion.Content>
+          </Accordion.Panel>
+        ))}
+      </Accordion>
     </>
-  )
-}
+  );
+};
